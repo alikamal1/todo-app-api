2e2afab5b6977f27596924720f048f8096c894ff
Ali Kamal <alikamal1@yahoo.com>
2019-06-29 20:08:13 +0300

    passprort

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,16 +10,26 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $req = Request::create(config('services.passport.login_endpoint'), 'POST', [
-            'grant_type' => 'password',
-            'client_id' => config('services.passport.client_id'),
-            'client_secret' => config('services.passport.client_secret'),
-            'username' => $request->username,
-            'password' => $request->password,
-        ]);
-
-        $response = app()->handle($req);
-        return $response;
+        $http = new \GuzzleHttp\Client;
+        try {
+            $response = $http->post(config('services.passport.login_endpoint'), [
+                'form_params' => [
+                    'grant_type' => 'password',
+                    'client_id' => config('services.passport.client_id'),
+                    'client_secret' => config('services.passport.client_secret'),
+                    'username' => $request->username,
+                    'password' => $request->password,
+                ]
+            ]);
+            return $response->getBody();
+        } catch (\GuzzleHttp\Exception\BadResponseException $e) {
+            if ($e->getCode() === 400) {
+                return response()->json('Invalid Request. Please enter a username or a password.', $e->getCode());
+            } else if ($e->getCode() === 401) {
+                return response()->json('Your credentials are incorrect. Please try again', $e->getCode());
+            }
+            return response()->json('Something went wrong on the server.', $e->getCode());
+        }
     }
     public function register(Request $request)
     {
